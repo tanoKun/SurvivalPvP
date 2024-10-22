a04970b21b2ec23546d028a9f50a968208d601a0
tanoKun <76431283+tanokun@users.noreply.github.com>
2024-10-22 23:02:43 +0900

    [Add] チーム間移動の追加

--- src/main/kotlin/com/github/tanokun/survivalpvp/command/setting/TeamSubCommand.kt
+++ src/main/kotlin/com/github/tanokun/survivalpvp/command/setting/TeamSubCommand.kt
@@ -3,18 +3,21 @@ package com.github.tanokun.survivalpvp.command.setting
 import com.github.tanokun.survivalpvp.game.team.TeamType
 import com.github.tanokun.survivalpvp.plguin
 import dev.jorel.commandapi.CommandPermission
-import dev.jorel.commandapi.kotlindsl.anyExecutor
-import dev.jorel.commandapi.kotlindsl.commandAPICommand
-import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.jorel.commandapi.kotlindsl.*
+import net.kyori.adventure.sound.Sound
 import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
 
 object TeamSubCommand {
+    private val soundExperience = Sound.sound(NamespacedKey.minecraft("entity.experience_orb.pickup"), Sound.Source.PLAYER, 3f, 1f)
+
     init {
         commandAPICommand("setting") {
             withPermission(CommandPermission.OP)
             subcommand("team") {
                 subcommand("random") {
-                    anyExecutor { _, _ ->
+                    anyExecutor { sender, _ ->
                         plguin.createTeam()
 
                         SettingCommand.generator.clearPlayers()
@@ -39,6 +42,23 @@ object TeamSubCommand {
 
                             plguin.getObjectiveTeam(team).addEntity(player)
                         }
+
+                        sender.playSound(soundExperience)
+                        sender.sendMessage("§7チームをシャッフルしました")
+                    }
+                }
+                subcommand("add") {
+                    playerArgument("target")
+                    multiLiteralArgument("team", literals = TeamType.entries.map { it.name }.toTypedArray())
+                    anyExecutor { sender, args ->
+                        val player = args.getOptionalByClass(0, Player::class.java).get()
+                        val team = TeamType.valueOf(args.getOrDefaultUnchecked(1, "BLUE"))
+
+                        TeamType.entries.forEach { plguin.getObjectiveTeam(it).removeEntity(player) }
+                        plguin.getObjectiveTeam(team).addEntity(player)
+
+                        sender.playSound(soundExperience)
+                        sender.sendMessage("§7「${player.name}」を「${team.display}§7」に追加しました")
                     }
                 }
             }
